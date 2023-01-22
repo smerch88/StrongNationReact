e65ae50a00b874032657e9e45e88485532c4087c
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-01-22 21:42:07 +0200

    add: component Timer

---
+++ src/components/Timer/Timer.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+function Timer() {
+  const [date, setDate] = useState(new Date());
+
+  const STARTDATE = new Date('1 January, 2023');
+  const now = new Date();
+  const distance = now - STARTDATE;
+  const days = Math.floor(distance / (24 * 60 * 60 * 1000));
+
+  function refreshTimer() {
+    setDate(new Date());
+  }
+
+  useEffect(() => {
+    const timerId = setInterval(refreshTimer, 1000);
+    return function cleanup() {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  return (
+    <span>
+      {days} днів {date.toLocaleTimeString()}
+    </span>
+  );
+}
+
+export default Timer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import Timer from 'components/Timer/Timer';
+
 const Home = () => {
   return (
     <div>
       <h1>Home page</h1>
+      <Timer />
     </div>
   );
 };
