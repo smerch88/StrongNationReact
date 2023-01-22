347af6c24427685d05b6a0c71d8437222ef980a9
smerch88 <maxarmyk@gmail.com>
2023-01-22 18:17:39 +0200

    basic structure of pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout';
+
+const HomePage = lazy(() => import('../pages/Home'));
+const DonatePage = lazy(() => import('../pages/Donate'));
+const AdminPage = lazy(() => import('../pages/Admin'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/donate" element={<DonatePage />} />
+          <Route path="*" element={<HomePage />} />
+          <Route path="/admin" element={<AdminPage />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
