a2c636d1e800b507b4060bcf63f6165d0241c130
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-01-24 19:02:00 +0200

    feat: AdminForm

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { Timer } from 'components/Timer/Timer';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h1>Home page</h1>
       <Timer />
+      <NavLink to="/admin">ADMIN</NavLink>
     </div>
   );
 };
