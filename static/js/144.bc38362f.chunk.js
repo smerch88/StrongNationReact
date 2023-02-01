1357595d12ecfc13bcd765fa6bcfbd0828daae28
KuznietsovaOksana <109915545+KuznietsovaOksana@users.noreply.github.com>
2023-02-01 19:16:25 +0000

    Deploying to gh-pages from @ smerch88/StrongNationReact@76235a72c08d13ab606eb2144c4912281653ee5e 🚀

---
+++ static/js/144.bc38362f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstrong_nation_react=self.webpackChunkstrong_nation_react||[]).push([[144],{5144:function(n,t,e){e.r(t);var a=e(1614),i=e(184);t.default=function(){return(0,i.jsx)(a.Z,{maxWidth:"sm",sx:{mt:"209px",paddingLeft:"86px",paddingRight:"86px"},children:(0,i.jsx)("h1",{children:"Donate page"})})}}}]);
+//# sourceMappingURL=144.bc38362f.chunk.js.map
