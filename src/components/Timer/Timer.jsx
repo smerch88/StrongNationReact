ab4f2a16289731594e6c425196af44b3fe3701fe
smerch88 <maxarmyk@gmail.com>
2023-02-03 18:22:06 +0200

    fix: timer date change

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 export const Timer = () => {
   const [date, setDate] = useState(new Date());
 
-  const STARTDATE = new Date('1 January, 2023');
+  const STARTDATE = new Date('24 February, 2022');
   const now = new Date();
   const distance = now - STARTDATE;
   const days = Math.floor(distance / (24 * 60 * 60 * 1000));
