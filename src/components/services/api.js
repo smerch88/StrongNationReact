ccf4b6335692f77a059984b710a9e7e2107bd9c6
smerch88 <maxarmyk@gmail.com>
2023-02-06 16:30:13 +0200

    fix: table resize

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,19 +1,6 @@
 import axios from 'axios';
 
-const starwars = axios.create({
-  baseURL: 'https://swapi.py4e.com/api/',
-  timeout: 1000,
-});
 export const regions = axios.create({
   baseURL: 'https://63e02a7f8b24964ae0fa52df.mockapi.io/Regions',
   timeout: 1000,
 });
-
-export const fetchStarWarsCharacter = async peopleName => {
-  try {
-    const { data } = await starwars.get(`people/?search=${peopleName}`);
-    return data;
-  } catch (error) {
-    return error.message;
-  }
-};
