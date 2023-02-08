1ca2c46db3d787caeac7afa89ea459c24a4b6e95
AntonVitulskyi <vitulskiyanton@gmail.com>
2023-02-09 00:23:13 +0200

    feat: adaptive contacts

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,6 +1,6 @@
-
 import React from 'react';
 
+import { EnvelopIcon, MapPointIcon, PhoneIcon } from './Icons';
 import {
   BigStyledBox,
   ContactsText,
@@ -12,28 +12,32 @@ import {
   StyledContainer,
   StyledLink,
 } from './Contacts.styled';
-import { EnvelopIcon, MapPointIcon, PhoneIcon } from './Icons';
+import { CardMedia } from '@mui/material';
 
-const Contact = () => {
+const Contacts = () => {
   return (
     <StyledContainer>
-      <ContactsTitle variant='h2' component='h1'>КОНТАКТИ</ContactsTitle>
+      <ContactsTitle variant="h2" component="h1">
+        КОНТАКТИ
+      </ContactsTitle>
       <BigStyledBox>
         <MiddleStyledBox>
           <SmallStyledBox>
-              <ContactsText variant='h5'>
-                Ви можете з нами зв’язатися за номером:
-              </ContactsText>
-              <IconBox>
+            <ContactsText variant="h5">
+              Ви можете з нами зв’язатися за номером:
+            </ContactsText>
+            <IconBox>
               <PhoneIcon />
-              <StyledLink href="tel:+380504711203">+380 50 471 1203</StyledLink>
+              <StyledLink variant="h5" href="tel:+380504711203">
+                +380 50 471 1203
+              </StyledLink>
             </IconBox>
           </SmallStyledBox>
           <SmallStyledBox>
-            <ContactsText>Та відправити повідомлення:</ContactsText>
+            <ContactsText variant="h5">Та відправити повідомлення:</ContactsText>
             <IconBox>
               <EnvelopIcon />
-              <StyledLink href="mailto:Braveukraine22@gmail.com">
+              <StyledLink variant="h5" href="mailto:Braveukraine22@gmail.com">
                 Braveukraine22@gmail.com
               </StyledLink>
             </IconBox>
@@ -41,26 +45,24 @@ const Contact = () => {
           <SmallStyledBox>
             <ContactsText>Наша адреса:</ContactsText>
             <IconBox>
-              <MapPointIcon />
+              <MapPointIcon/>
               <ContactsValueText>
-                Провулок Хомова Ярослава, буд. 14А Киев, Украина 03035
+                Провулок Хомова Ярослава, буд. 14А, Київ, Україна 03035
               </ContactsValueText>
             </IconBox>
           </SmallStyledBox>
         </MiddleStyledBox>
-        <iframe
-          title="map"
-          src="https://maps.google.com/maps?q=123 Main St, City, State 12345&t=&z=13&ie=UTF8&iwloc=&output=embed"
-          width="324px"
+        <CardMedia
+          sx={{borderRadius: '15px', border: '1px solid #455A64'}}
+          component="iframe"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d800.4730239517309!2d30.478612906477675!3d50.434706520640674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4ce9528100001%3A0x5c7e707ee7f02735!2z0L_QtdGALiDQndC40LrQvtC70LDRjyDQntGB0YLRgNC-0LLRgdC60L7Qs9C-LCAxNNCwLCDQmtC40LXQsiwgMDMwMzU!5e0!3m2!1sru!2sua!4v1675888242126!5m2!1sru!2sua"
+          width="100%"
           height="200px"
-          style={{ borderRadius: '15px' }}
-          allowFullScreen
-          aria-hidden="false"
-          tabIndex="0"
+          loading="lazy"
         />
       </BigStyledBox>
     </StyledContainer>
   );
 };
 
-export default Contact;
+export default Contacts;
--- src/pages/Contacts/Contacts.styled.js
+++ src/pages/Contacts/Contacts.styled.js
@@ -5,7 +5,12 @@ export const StyledContainer = styled(Container)(({ theme }) => ({
     flexDirection: 'column',
     alignItems: 'center',
     padding: '31px 18px',
-    margin: 0,
+    margin: '0 auto',
+width: '100%',
+    [theme.breakpoints.up('tablet')]: {
+      padding: '51px 59px',
+    },
+
     [theme.breakpoints.up('desktop')]: {
       flexDirection: 'row',
       marginBottom: '60px',
@@ -17,7 +22,17 @@ export const SmallStyledBox = styled(Box)(({ theme }) => ({
     flexDirection: 'column',
     alignItems: 'flex-start',
     justifyContent: 'center',
-    marginBottom: '20px',
+    marginTop: '20px',
+
+    [theme.breakpoints.up('tablet')]: {
+      marginTop: '30px',
+      flexDirection: 'row',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      backgroundColor: 'red',
+      width: '100%'
+    },
+
     [theme.breakpoints.up('desktop')]: {
       flexDirection: 'row',
       marginBottom: '60px',
@@ -29,7 +44,12 @@ export const SmallStyledBox = styled(Box)(({ theme }) => ({
     flexDirection: 'column',
     alignItems: 'flex-start',
     justifyContent: 'center',
-    marginBottom: '20px',
+    marginBottom: '25px',
+
+    [theme.breakpoints.up('tablet')]: {
+
+    },
+
     [theme.breakpoints.up('desktop')]: {
       flexDirection: 'row',
       marginBottom: '60px',
@@ -41,7 +61,11 @@ export const BigStyledBox = styled(Box)(({ theme }) => ({
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: '20px',
+
+    [theme.breakpoints.up('tablet')]: {
+
+    },
+
     [theme.breakpoints.up('desktop')]: {
       flexDirection: 'row',
       marginBottom: '60px',
@@ -52,14 +76,21 @@ export const BigStyledBox = styled(Box)(({ theme }) => ({
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
-    [theme.breakpoints.up('desktop')]: {
-      flexDirection: 'row',
-      marginBottom: '60px',
+    justifyContent: 'flex-start',
+    marginTop: '20px',
+
+    [theme.breakpoints.up('tablet')]: {
+      margin: '0px',
     },
   }));
 
 export const ContactsTitle = styled(Typography)(({ theme }) => ({
+
+
+  [theme.breakpoints.up('tablet')]: {
+    fontSize: '40px',
+  },
+
     [theme.breakpoints.up('desktop')]: {
       display: 'inline-block',
     },
@@ -70,13 +101,28 @@ export const ContactsTitle = styled(Typography)(({ theme }) => ({
     fontStyle: 'normal',
     fontSize: '20px',
     lineHeight: '23px',
-    color: '#1F2528',
+    color: theme.palette.colorList.almostblack,
+
+    [theme.breakpoints.up('tablet')]: {
+      fontSize: '18px',
+      lineHeight: '21px',
+      width: '300px'
+    },
+
     [theme.breakpoints.up('desktop')]: {
       display: 'inline-block',
     },
   }));
 
   export const ContactsValueText = styled(Typography)(({ theme }) => ({
+    padding: '0',
+    width: '80%',
+    textAlign: 'start',
+    textTransform: 'none',
+    fontStyle: 'normal',
+    fontSize: '18px',
+    lineHeight: '21px',
+    color: theme.palette.colorList.almostblack,
     [theme.breakpoints.up('desktop')]: {
       display: 'inline-block',
     },
@@ -84,11 +130,25 @@ export const ContactsTitle = styled(Typography)(({ theme }) => ({
 
 
   export const StyledLink = styled(Link)(({ theme }) => ({
+    textTransform: 'none',
+    fontStyle: 'normal',
+    fontSize: '18px',
+    lineHeight: '21px',
+    color: theme.palette.colorList.almostblack,
     [theme.breakpoints.up('desktop')]: {
       display: 'inline-block',
     },
   }));
 
 
+  export const SvgBox = styled('div')(({ theme }) => ({
+    marginRight: '16px',
+    width: '42px',
+    height: '42px',
+    [theme.breakpoints.up('tablet')]: {
+      width: '32px',
+      height: '32px',
+    },
+  }));
 
- 
+
