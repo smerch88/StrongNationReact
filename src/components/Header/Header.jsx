cef17e56d089f3850996a35dffb6ad06144cfc47
smerch88 <maxarmyk@gmail.com>
2023-02-09 22:47:50 +0200

    feat: header refactor

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,44 +1,9 @@
-import {
-  AppBar,
-  Box,
-  Button,
-  Container,
-  styled,
-  useMediaQuery,
-} from '@mui/material';
-import { ReactComponent as ReactLogo } from './pictures/header_logo.svg';
-import { ReactComponent as Burger } from './pictures/burger.svg';
+import { Box, Button, Container, useMediaQuery } from '@mui/material';
+
 import { Link } from 'react-router-dom';
+
 import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
-
-const StyledLogo = styled(ReactLogo)(({ theme }) => ({
-  [theme.breakpoints.up('tablet')]: {
-    width: '156px',
-    height: '53px',
-  },
-  [theme.breakpoints.up('desktop')]: {
-    width: '152px',
-    height: '67px',
-  },
-}));
-
-const StyledBurger = styled(Burger)(({ theme }) => ({}));
-
-const StyledHeader = styled(AppBar)(({ theme }) => ({
-  position: 'static',
-
-  background: '#FFFFFF',
-  opacity: '0.7',
-  boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-
-  paddingTop: '10px',
-  paddingBottom: '10px',
-
-  [theme.breakpoints.up('tablet')]: {
-    paddingTop: '24px',
-    paddingBottom: '24px',
-  },
-}));
+import { StyledBurger, StyledHeader, StyledLogo } from './Header.Styled';
 
 const pages = [
   { name: 'новини', link: '/news' },
