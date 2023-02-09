b02f5808311d84ca00a3fdbe53cb8fa939c67121
smerch88 <maxarmyk@gmail.com>
2023-02-09 22:47:59 +0200

    fix: slider fix

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -5,6 +5,7 @@ import { AutoPlayContainer, StyledImg } from './Slider.styled';
 
 export const AutoPlay = ({ ...props }) => {
   const settings = {
+    slidesToShow: 5,
     dots: false,
     infinite: true,
     autoplay: true,
@@ -18,7 +19,6 @@ export const AutoPlay = ({ ...props }) => {
         breakpoint: 1366,
         settings: {
           slidesToShow: 5,
-          infinite: true,
         },
       },
       {
