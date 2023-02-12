7e6616d7792d29b61db14ef860dc5699ac0e4ec7
smerch88 <maxarmyk@gmail.com>
2023-02-12 16:36:39 +0200

    feat: update structure for circles

--- src/components/ReadMore/Circles.jsx
+++ src/components/ReadMore/Circles.jsx
@@ -1,11 +1,11 @@
-import { Box, styled } from '@mui/material';
+import { Box, styled, Typography } from '@mui/material';
 
 const Circle = styled('li')(({ theme }) => ({
   borderRadius: '50%',
-  width: '150px',
-  height: '150px',
+  width: '122px',
+  height: '122px',
 
-  background: 'rgba(132, 148, 100, 0.6)',
+  background: ' rgba(69, 90, 100, 0.7);',
   border: '1px solid #FFFFFF',
 
   textAlign: 'center',
@@ -23,20 +23,30 @@ const CirclesUl = styled('ul')(({ theme }) => ({
 }));
 
 export const Circles = ({ oblList, currentId }) => {
+  console.log(oblList, 'oblList Circles');
+  console.log(currentId, 'oblList currentId');
   return (
     <>
       <Box>
         <CirclesUl>
-          {oblList
-            .filter(item => item.id === currentId)
-            .map(item => (
-              <div style={{ display: 'flex' }}>
-                <Circle>{item.region}</Circle>
-                <Circle>{item.feat1}</Circle>
-                <Circle>{item.feat2}</Circle>
-                <Circle>{item.feat3}</Circle>
-              </div>
-            ))}
+          {oblList &&
+            oblList
+              .filter(item => Number(item.id) === Number(currentId))
+              .map(item => (
+                <div
+                  style={{ display: 'flex', flexWrap: 'wrap' }}
+                  key={item.id}
+                >
+                  <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                    <Typography>витрачено на потреби вiйськових</Typography>
+                    <Circle>{item?.categories[0]?.number ?? 0}</Circle>
+                  </div>
+                  <div>
+                    <Circle>{item?.categories[1]?.number ?? 0}</Circle>
+                    <Typography>витрачено на потреби громадян</Typography>
+                  </div>
+                </div>
+              ))}
         </CirclesUl>
       </Box>
     </>
