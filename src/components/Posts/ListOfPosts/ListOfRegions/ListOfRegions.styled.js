0e5c64d961d80fa1ce31a5c7e34494ffcc92148b
Daniel-Slavnyi <dslavnyj00@gmail.com>
2023-02-22 16:30:34 +0200

    feat: active region on filter

--- src/components/Posts/ListOfPosts/ListOfRegions/ListOfRegions.styled.js
+++ src/components/Posts/ListOfPosts/ListOfRegions/ListOfRegions.styled.js
@@ -36,6 +36,8 @@ export const WrapOfRegions = styled('div')(({ theme }) => ({
 }));
 
 export const Btn = styled('button')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
   background: 'transparent',
   border: 'none',
   textTransform: 'uppercase',
@@ -48,9 +50,39 @@ export const Btn = styled('button')(({ theme }) => ({
   cursor: 'pointer',
   color: '#1F2528',
 
+  // '&:before': {
+  //   content: '""',
+  //   display: 'block',
+  //   marginRight: '3px',
+  //   width: '4px',
+  //   height: '4px',
+  //   background: '#748E9A',
+  //   borderRadius: '50px',
+  // },
+
   '&:hover': {
     color: '#748E9A',
   },
+  //   [theme.breakpoints.up('tablet')]: {
+  //     width: 'calc((100% - 50px) / 3)',
+  //     padding: '0 0 14px 0',
+  //     overflow: 'hidden',
+  //     display: 'flex',
+  //     flexDirection: 'column',
+  //     justifyContent: 'space-between',
+  //   },
+  //   [theme.breakpoints.up('desktop')]: {
+  //     width: 'calc((100% - 72px) / 3)',
+  //   },
+}));
+
+export const SpanEl = styled('span')(({ theme }) => ({
+  display: 'block',
+  marginRight: '3px',
+  width: '4px',
+  height: '4px',
+  background: '#748E9A',
+  borderRadius: '50px',
   //   [theme.breakpoints.up('tablet')]: {
   //     width: 'calc((100% - 50px) / 3)',
   //     padding: '0 0 14px 0',
